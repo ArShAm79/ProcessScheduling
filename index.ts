2ea9624e4819047a65d9e4438213d5d7df68df58
Arsham79 <arsham.ramezani@gmail.com>
2022-07-16 13:30:57 +0430

    fix: bug resolved

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
+import FCFS from './functions/FCFS'
 import { GenerateRandomNumbersPriority } from './functions/GenerateRandomNumbers'
 import Priority from './functions/Priority'
+import SJF from './functions/SJF'
+import SRT from './functions/SRT'
 
 const Index = () => {
   const numbers = GenerateRandomNumbersPriority(20)
-  Priority(numbers)
+  const priorityNumbers = numbers.map((item) => ({ ...item }))
+  const SJFNumbers = numbers.map((item) => ({ ...item }))
+  const SRTNumbers = numbers.map((item) => ({ ...item }))
+  const FCFSNumbers = numbers.map((item) => ({ ...item }))
+  Priority(priorityNumbers)
+  SJF(SJFNumbers)
+  SRT(SRTNumbers)
+  FCFS(FCFSNumbers)
 }
 Index()
